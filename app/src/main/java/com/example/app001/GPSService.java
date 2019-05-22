0c8532f85c1d0d0c5cd6e5be3b5a7476dc99a0a5
unknown <ghetaohmaina2@gmail.com>
2019-05-22 15:20:57 +0300

    added a location Service in the reycler view

---
+++ app/src/main/java/com/example/app001/GPSService.java
@@ -0,0 +1,233 @@
+package com.example.app001;
+
+import android.app.AlertDialog;
+import android.app.Service;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Binder;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
+
+public class GPSService extends Service implements LocationListener {
+    private final IBinder binder = new GPSBinder();
+    // saving the context for later use
+    private Context mContext;
+
+    public GPSService() {
+
+    }
+
+    // if GPS is enabled
+    boolean isGPSEnabled = false;
+    // if Network is enabled
+    boolean isNetworkEnabled = false;
+    // if Location co-ordinates are available using GPS or Network
+    public boolean isLocationAvailable = false;
+
+    // Location and co-ordinates coordinates
+    Location mLocation;
+    double mLatitude;
+    double mLongitude;
+
+    // Minimum time fluctuation for next update (in milliseconds)
+    private static final long TIME = 30000;
+    // Minimum distance fluctuation for next update (in meters)
+    private static final long DISTANCE = 20;
+
+    // Declaring a Location Manager
+    protected LocationManager mLocationManager;
+
+    /* addition*/
+
+    public GPSService(Context context) {
+        this.mContext = context;
+        mLocationManager = (LocationManager) mContext
+                .getSystemService(LOCATION_SERVICE);
+
+    }
+
+    /**
+     * Returs the Location
+     *
+     * @return Location or null if no location is found
+     */
+    public Location getLocation() {
+        try {
+
+            // Getting GPS status
+            isGPSEnabled = mLocationManager
+                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            // If GPS enabled, get latitude/longitude using GPS Services
+            if (isGPSEnabled) {
+                mLocationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER, TIME, DISTANCE, this);
+                if (mLocationManager != null) {
+                    mLocation = mLocationManager
+                            .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    if (mLocation != null) {
+                        mLatitude = mLocation.getLatitude();
+                        mLongitude = mLocation.getLongitude();
+                        isLocationAvailable = true; // setting a flag that
+                        // location is available
+                        return mLocation;
+                    }
+                }
+            }
+
+            // If we are reaching this part, it means GPS was not able to fetch
+            // any location
+            // Getting network status
+            isNetworkEnabled = mLocationManager
+                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (isNetworkEnabled) {
+                mLocationManager.requestLocationUpdates(
+                        LocationManager.NETWORK_PROVIDER, TIME, DISTANCE, this);
+                if (mLocationManager != null) {
+                    mLocation = mLocationManager
+                            .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    if (mLocation != null) {
+                        mLatitude = mLocation.getLatitude();
+                        mLongitude = mLocation.getLongitude();
+                        isLocationAvailable = true; // setting a flag that
+                        // location is available
+                        return mLocation;
+                    }
+                }
+            }
+            // If reaching here means, we were not able to get location neither
+            // from GPS not Network,
+            if (!isGPSEnabled) {
+                // so asking user to open GPS
+                askUserToOpenGPS();
+            }
+
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
+        // if reaching here means, location was not available, so setting the
+        // flag as false
+        isLocationAvailable = false;
+        return null;
+    }
+
+    /**
+     * Gives you complete address of the location
+     *
+     * @return complete address in String
+     */
+
+
+
+
+    /**
+     * get latitude
+     *
+     * @return latitude in double
+     */
+    public double getLatitude() {
+        if (mLocation != null) {
+            mLatitude = mLocation.getLatitude();
+        }
+        return mLatitude;
+    }
+
+    /**
+     * get longitude
+     *
+     * @return longitude in double
+     */
+    public double getLongitude() {
+        if (mLocation != null) {
+            mLongitude = mLocation.getLongitude();
+        }
+        return mLongitude;
+    }
+
+    /**
+     * close GPS to save battery
+     */
+    public void closeGPS() {
+        if (mLocationManager != null) {
+            mLocationManager.removeUpdates(GPSService.this);
+        }
+    }
+
+    /**
+     * show settings to open GPS
+     */
+    public void askUserToOpenGPS() {
+        AlertDialog.Builder mAlertDialog = new AlertDialog.Builder(mContext);
+
+        // Setting Dialog Title
+        mAlertDialog.setTitle("Location not available, Open GPS?")
+                .setMessage("Activate GPS to use use location services?")
+                .setPositiveButton("Open Settings", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        mContext.startActivity(intent);
+                    }
+                })
+                .setNegativeButton("Cancel",new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                }).show();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mContext = getApplicationContext();
+        mLocationManager = (LocationManager) mContext
+                .getSystemService(LOCATION_SERVICE);
+        getLocation();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        return START_STICKY;
+    }
+
+    /**
+     * Updating the location when location changes
+     */
+    @Override
+    public void onLocationChanged(Location location) {
+        mLatitude = location.getLatitude();
+        mLongitude = location.getLongitude();
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return binder;
+    }
+
+    public class GPSBinder extends Binder {
+        GPSService getService() {
+
+            return GPSService.this;
+
+        }
+    }
+
+}
--- app/src/main/java/com/example/app001/RecyclerActivity.java
+++ app/src/main/java/com/example/app001/RecyclerActivity.java
@@ -1,11 +1,33 @@
 package com.example.app001;
 
+import android.Manifest;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+//
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
-public class RecyclerActivity extends AppCompatActivity {
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+public class RecyclerActivity extends AppCompatActivity  implements View.OnClickListener{
 
     //handler for recycler view
     private RecyclerView recyclerView;
@@ -16,6 +38,28 @@ public class RecyclerActivity extends AppCompatActivity {
 
     //layout manager
     private  RecyclerView.LayoutManager layoutManager;
+
+    //LOCATION ADDS
+    Button LocButton;
+    boolean mBound = false;
+    GPSService mService;
+    private ServiceConnection connection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            GPSService.GPSBinder binder = (GPSService.GPSBinder) service;
+            mService = binder.getService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mBound = false;
+        }
+    };
+    //LOCATION ENDS
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +70,119 @@ public class RecyclerActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         recyclerAdapter = new RecyclerAdapter(images);
         recyclerView.setAdapter(recyclerAdapter);
+        //LOCATION ADDS
+        LocButton = findViewById(R.id.btnloc);
+        LocButton.setOnClickListener(this);
 
+        Intent intent = new Intent(this, GPSService.class);
+        bindService(intent, connection, Context.BIND_AUTO_CREATE);
+        //Request Permissions
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //Ask for permission ANdroid L >
+            ActivityCompat.requestPermissions(RecyclerActivity.this, new String[]{ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION,}, 11);
+        }
+        //LOCATION ENDS
+
+    }
+    //LOCATION ADDS
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.btnloc:
+                if (mBound) {
+                    Location loc = mService.getLocation();
+                    Toast.makeText(this, "The location is " + getLocationAddress(loc.getLatitude(), loc.getLongitude()), Toast.LENGTH_LONG).show();
+                    Log.d("Location is", loc.toString());
+                }
+            break;
+        }
+
+        /*if (v.getId() == R.id.btnloc) {
+            if (mBound) {
+                Location loc = mService.getLocation();
+                Toast.makeText(this, "The location is " + getLocationAddress(loc.getLatitude(), loc.getLongitude()), Toast.LENGTH_LONG).show();
+                Log.d("Location is", loc.toString());
+            }
+        }*/
+
+    }
+    // METHOD FOR ACCESSING LOCATION
+    public String getLocationAddress(double mLatitude, double mLongitude) {
+
+
+        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        // Get the current location from the input parameter list
+        // Create a list to contain the result address
+        List<Address> addresses = null;
+        try {
+            /*
+             * Return 1 address.
+             */
+            addresses = geocoder.getFromLocation(mLatitude, mLongitude, 1);
+        } catch (IOException e1) {
+            e1.printStackTrace();
+            return ("IO Exception trying to get address:" + e1);
+        } catch (IllegalArgumentException e2) {
+            // Error message to post in the log
+            String errorString = "Illegal arguments "
+                    + mLatitude + " , "
+                    + mLongitude
+                    + " passed to address service";
+            e2.printStackTrace();
+            return errorString;
+        }
+        // If the reverse geocode returned an address
+        if (addresses != null && addresses.size() > 0) {
+            // Get the first address
+            Address address = addresses.get(0);
+            /*
+             * Format the first line of address (if available), city, and
+             * country name.
+             */
+            String addressText = String.format(
+                    "%s, %s, %s",
+                    // If there's a street address, add it
+                    address.getMaxAddressLineIndex() > 0 ? address
+                            .getAddressLine(0) : "",
+                    // Locality is usually a city
+                    address.getLocality(),
+                    // The country of the address
+                    address.getCountryName());
+            // Return the text
+            return addressText;
+        } else {
+            return "No address found by the service: Note to the developers, If no address is found by google itself, there is nothing you can do about it.";
+        }
+
+
+    }
+
+    //LOCATION ENDS
+
+    //OVERRIDES
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Intent intent = new Intent(this, GPSService.class);
+        bindService(intent, connection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unbindService(connection);
+        mBound = false;
     }
 }
