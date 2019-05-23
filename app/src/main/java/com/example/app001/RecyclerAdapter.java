a096767ce59105c57f34c7c6b1434133d8c089eb
Neptune <eugeneogongo@live.com>
2019-05-23 21:52:48 +0300

    Added Event Listeners to Card and Images

--- app/src/main/java/com/example/app001/RecyclerAdapter.java
+++ app/src/main/java/com/example/app001/RecyclerAdapter.java
@@ -1,5 +1,6 @@
 package com.example.app001;
 
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,15 +9,20 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.app001.Model.PanicEvent;
+
+import org.greenrobot.eventbus.EventBus;
+
+import java.util.List;
+
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ImageViewHolder> {
     //
     Button BtnLoc ;
     // a constructor which will accept an array of images
-    private int[] images;
+    List<PanicEvent> panicEventList;
 
-    public RecyclerAdapter(int[] images){
-        this.images= images;
-
+    public RecyclerAdapter(List<PanicEvent> panicEventList) {
+        this.panicEventList = panicEventList;
     }
 
     @Override
@@ -32,15 +38,30 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ImageV
     @Override
     public void onBindViewHolder( ImageViewHolder viewHolder, int position) {
         // ensures the appropriate image and text are positioned in place
-        int image_id = images[position];
-        viewHolder.Terror.setImageResource(image_id);
-        viewHolder.Terror_nm.setText("image"+ position);
+        final PanicEvent event = panicEventList.get(position);
+        viewHolder.Terror.setImageResource(event.getImage());
+        viewHolder.Terror.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Post event to all activity using Eventbus.
+                EventBus.getDefault().post(event);
+            }
+        });
+        //The whole card is clickable
+        viewHolder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Post event to all activity using Eventbus.
+                EventBus.getDefault().post(event);
+            }
+        });
+        viewHolder.Terror_nm.setText(event.getCaption());
     }
 
     @Override
     public int getItemCount() {
         //return length of the array as its the number of our objects
-        return images.length;
+        return panicEventList.size();
     }
     //all items in recycler view are objects of view holder a class to create this objects
     public static class ImageViewHolder extends RecyclerView.ViewHolder{
@@ -48,11 +69,12 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ImageV
         // each object in the view has an image and a text ,create the two holders
         ImageView Terror;
         TextView Terror_nm;
-
+        CardView cardView;
         public ImageViewHolder( View itemView) {
             super(itemView);
             Terror = itemView.findViewById(R.id.terror);
             Terror_nm = itemView.findViewById(R.id.terror_nm);
+            cardView = itemView.findViewById(R.id.cardevent);
         }
     }
 
